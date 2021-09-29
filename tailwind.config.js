b8e26e222b4c50f68f73443c94c44470705f4439
Kyle Mayer <x@kylemayer.dev>
2021-09-29 12:02:13 -0700

    tw plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 module.exports = {
-  purge: [],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter var",],
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [
+      require("@tailwindcss/aspect-ratio"),
+      require("@tailwindcss/forms"),
+      require("@tailwindcss/typography"),
+    ],
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+};
